93bb5e1dc4ef9641990d464c9d6b4f0b9851556d
buylan <buylan01@gmail.com>
2026-07-27 19:23:29 +0800

    refactor(layout): migrate action list item to ConstraintLayout (master)
    - Replace RelativeLayout with ConstraintLayout in kr_action_list_item.xml
    - Add constraintlayout dependency in core/build.gradle.kts
    - Constrain all views with layout_constraint* attributes
    - Adjust padding and margins for consistent item spacing

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -31,6 +31,7 @@ android {
 
 dependencies {
     implementation(libs.androidx.appcompat)
+    implementation(libs.constraintlayout)
     implementation(libs.material)
     implementation(project(":common"))
 }
