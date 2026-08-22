14b8b34596c1403ba26b7747a74f79553f196d29
buylan <buylan01@gmail.com>
2026-08-22 13:44:27 +0800

    refactor(samples): consolidate config path scripts (master)

---
+++ app/src/main/assets/samples/env/config_path.sh
@@ -0,0 +1,15 @@
+printf "页面配置来源 (文件) \nPAGE_CONFIG_DIR=\n"
+echo "$PAGE_CONFIG_DIR"
+echo ""
+
+printf "页面配置来源 (目录) \nPAGE_CONFIG_DIR=\n"
+echo "$PAGE_CONFIG_FILE"
+echo ""
+
+printf "页面配置工作位置 (目录) \nPAGE_CONFIG_DIR=\n"
+echo "$PAGE_WORK_DIR"
+echo ""
+
+printf "页面配置工作位置 (文件) \nPAGE_CONFIG_DIR=\n"
+echo "$PAGE_WORK_FILE"
+echo ""
