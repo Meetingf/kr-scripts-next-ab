93d2a83585735019e003659c88b6ff84623ac8cc
Meetingf <1779510253@qq.com>
2026-08-25 16:45:19 +0800

    Update DialogLogFragment.kt

--- core/src/main/java/com/krscripts/core/ui/dialog/DialogLogFragment.kt
+++ core/src/main/java/com/krscripts/core/ui/dialog/DialogLogFragment.kt
@@ -11,6 +11,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.text.buildSpannedString
 import androidx.core.text.color
