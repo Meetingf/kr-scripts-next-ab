a59736625563e78ea84de8d08be16f5701aafe51
Meetingf <1779510253@qq.com>
2026-08-25 17:17:19 +0800

    更新 DialogLogFragment.kt

--- core/src/main/java/com/krscripts/core/ui/dialog/DialogLogFragment.kt
+++ core/src/main/java/com/krscripts/core/ui/dialog/DialogLogFragment.kt
@@ -228,7 +228,16 @@ class DialogLogFragment : DialogFragment() {
             ShellLogType.OUTPUT_ERROR -> colorOutputError
             ShellLogType.INPUT -> colorInput
         }
-        return AnsiToSpannable.parse(text + "\n", defaultColor)
+        // 回车 \r 表示原地刷新（如进度百分比），需覆盖当前行而非追加新行
+        if (event.text.contains('\r')) {
+            val editable = outputView.editableText
+            val lineStart = editable.toString().lastIndexOf('\n') + 1
+            editable.delete(lineStart, editable.length)
+            // 只保留最后一次 \r 刷新后的内容
+            outputView.append(AnsiToSpannable.parse(event.text.substringAfterLast('\r'), defaultColor))
+        } else {
+            outputView.append(AnsiToSpannable.parse(event.text + "\n", defaultColor))
+        }
     }
 
     companion object {
